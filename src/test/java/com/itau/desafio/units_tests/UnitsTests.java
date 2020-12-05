52192affcff24dcba0346602cd014a3b455c6e74
NirsoItu <48810680+NirsoItu@users.noreply.github.com>
2020-12-05 19:11:12 -0200

    criaçãoda classe de realiza os testes unitários de todas as requisições e métodos dos services e dos controllers

---
+++ src/test/java/com/itau/desafio/units_tests/UnitsTests.java
@@ -0,0 +1,112 @@
+package com.itau.desafio.units_tests;
+
+import com.itau.desafio.model.TransactionModel;
+import com.itau.desafio.service.TransactionService;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.OffsetDateTime;
+
+import static org.junit.Assert.assertEquals;
+
+public class UnitsTests extends AbstractTest{
+    @Override
+
+    @Before
+
+    public void setUp() {
+        super.setUp();
+    }
+
+    // Teste para verificar a requisição da lista de transações
+    @Test
+    public void getTransactionListTest() throws Exception {
+        String uri = "/transacao";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+    }
+
+    // Teste para verificar a requisição de criação de uma transação
+    @Test
+    public void createTransactionTest() throws Exception {
+        TransactionService transactionService = new TransactionService();
+        TransactionModel transacao = new TransactionModel();
+
+        String uri = "/transacao";
+
+        transacao.setValor(55.00f);
+        transacao.setDataHora(OffsetDateTime.parse("2020-12-01T15:01:00-03:00"));
+        transactionService.addTransaction(transacao);
+
+        String inputJson = super.mapToJson(transacao);
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(inputJson))
+                .andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        //assertEquals(201, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(content, "");
+        System.out.println(content);
+    }
+
+    // Teste para verificar a requisição remoção das transações
+    @Test
+    public void deleteTransactionTest() throws Exception {
+        String uri = "/transacao";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(content, "Todas as transações foram apagadas com sucesso ");
+    }
+
+    // Teste para verificar a validação da regra de negócio (Igual ou menor a zero)
+    @Test
+    public void isTransactionUnderZeroTest() throws Exception {
+        TransactionService transactionService = new TransactionService();
+        TransactionModel transactionModel = new TransactionModel();
+
+        // Coloque qualquer valor negativo ou zero que retornara erro
+        transactionModel.setValor(1.0f);
+        transactionModel.setDataHora(OffsetDateTime.parse("2020-12-01T15:01:00-03:00"));
+
+        boolean expResult = false;
+        boolean result = transactionService.isTransactionUnderZero(transactionModel);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void isTransactionInFutureTest() throws Exception {
+        TransactionService transactionService = new TransactionService();
+        TransactionModel transactionModel = new TransactionModel();
+
+        transactionModel.setValor(1.0f);
+        // Coloque qualquer data ou horario acima do atual que retornara erro
+        transactionModel.setDataHora(OffsetDateTime.parse("2020-12-05T15:01:00-03:00"));
+
+        boolean expResult = false;
+        boolean result = transactionService.isTransactionInFuture(transactionModel);
+        assertEquals(expResult, result);
+    }
+
+    // Teste para verificar a requisição da lista de estatisticas
+    @Test
+    public void getStatisticsListTest() throws Exception {
+        String uri = "/estatistica";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+    }
+
+}
