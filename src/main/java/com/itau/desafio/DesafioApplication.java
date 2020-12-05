0377fd5040085dd137729ba6c438c9346f01bc3b
NirsoItu <48810680+NirsoItu@users.noreply.github.com>
2020-12-05 12:47:07 -0200

    inserindo logs na inicialização do programa

--- src/main/java/com/itau/desafio/DesafioApplication.java
+++ src/main/java/com/itau/desafio/DesafioApplication.java
@@ -1,13 +1,20 @@
 package com.itau.desafio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DesafioApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(DesafioApplication.class);
+
 	public static void main(String[] args) {
+
+		logger.info("Iniciando a API Itaú Unibanco - Desafio de Programação");
 		SpringApplication.run(DesafioApplication.class, args);
+		logger.info("API iniciada e pronta para receber requisições");
 	}
 
 }
