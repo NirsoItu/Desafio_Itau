ee23aaeec076fd1f9127b12c69919e8864d3bcc9
NirsoItu <48810680+NirsoItu@users.noreply.github.com>
2020-12-06 19:17:59 -0200

    correção das definições dos campos no swagger

--- src/main/java/com/itau/desafio/controller/TimeController.java
+++ src/main/java/com/itau/desafio/controller/TimeController.java
@@ -2,18 +2,18 @@ package com.itau.desafio.controller;
 
 import com.itau.desafio.model.TimeModel;
 import com.itau.desafio.service.TimeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
+@Api(value = "API Rest Transações e Estatísticas")
+@CrossOrigin(origins = "*")
 public class TimeController {
 
     @Autowired
@@ -21,6 +21,7 @@ public class TimeController {
 
     // Endpoint para buscar o tempo de estatística
     @RequestMapping(method = RequestMethod.GET, value = "/tempo")
+    @ApiOperation(value = "Retorna um tempo em segundos para estatísca atual")
     public List<TimeModel> getSeconds() {
         timeService.getSeconds();
         return timeService.getSeconds();
@@ -28,6 +29,7 @@ public class TimeController {
 
     // Endpoint para incluir um tempo de estatística
     @RequestMapping(method = RequestMethod.POST, value = "/tempo")
+    @ApiOperation(value = "Cria um tempo em segundos para estatísca atual")
     public ResponseEntity<?> post(@RequestBody TimeModel timeModel) throws Exception {
         timeService.addSeconds(timeModel);
         return ResponseEntity.status(HttpStatus.CREATED).body("O tempo foi incluído com sucesso! ");
